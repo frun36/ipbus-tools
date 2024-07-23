233240af34b119d78a063613089e8605cb63e714
frun36 <franek.urbanski@gmail.com>
2024-07-23 10:58:05 +0200

    Add SwtSequence

--- swt/swt.py
+++ swt/swt.py
@@ -7,7 +7,7 @@ class TransactionType(Enum):
     WRITE_AND = 0b011
     READ_SUM = 0b100
 
-class SwtCommand:
+class SwtWord:
     def __init__(self, type, address, data = 0):
         self.type = type
         self.address = address
@@ -38,5 +38,25 @@ class SwtCommand:
     def __str__(self) -> str:
         return '0x' + ''.join(map(lambda x : f"{x:02x}", self.get_bytes()))
 
-cmd = SwtCommand(TransactionType.WRITE_AND, 0xbadf00db, 0xadc0ffee)
-print(cmd)
+
+class SwtSequence:
+    def __init__(self, *commands):
+        self.commands = list(commands)
+
+    def __str__(self) -> str:
+        result = ''
+        for command in self.commands:
+            result += f'{command},write\n'
+            match command.type:
+                case TransactionType.READ | TransactionType.READ_OR | TransactionType.READ_SUM:
+                    result += 'read\n' # ignores read timeouts
+                case TransactionType.WRITE | TransactionType.WRITE_AND:
+                    pass
+                case _:
+                    raise ValueError("Invalid transaction type value")
+        return result
+
+cmd1 = SwtWord(TransactionType.READ_OR, 0xbadf00db, 0xadc0ffee)
+cmd2 = SwtWord(TransactionType.WRITE_AND, 0xbadf00d0)
+seq = SwtSequence(cmd1, cmd2)
+print(seq)
