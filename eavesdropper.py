7268e9bfe3951ea07a7d0b53b95c2410b759a4ee
frun36 <franek.urbanski@gmail.com>
2024-07-31 09:50:33 +0200

    Update eavesdropper to perform packet filtering

--- eavesdropper.py
+++ eavesdropper.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-import socket
 import sys
 import datetime
 
 import pyshark
 import ipbus_parser
+
+
 UDP_IP = "172.20.75.180"
 UDP_PORT = 50001
 
@@ -15,14 +16,21 @@ for packet in capture.sniff_continuously():
     try:
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f")
         packet_bytes = bytes.fromhex(packet.data.data)
-        print(ipbus_parser.Packet.from_bytes(packet_bytes)) # This assumes little endianness
+        ipb_packet = ipbus_parser.Packet.from_bytes(packet_bytes)
+        ipbus_parser.PacketTagger.tag_packet(ipb_packet)
         print(f"{packet.ip.src}")
+        print(ipb_packet)
+
         if packet.ip.src == UDP_IP:
-            filename = f"packets/{timestamp}_res.bin"
+            filename = f"{timestamp}_res.bin"
         else:
-            filename = f"packets/{timestamp}_req.bin"
-        with open(filename, 'ab') as f:
+            filename = f"{timestamp}_req.bin"
+        
+        with open("packets/" + filename, 'ab') as f:
             f.write(packet_bytes)
+        if ipb_packet.label == "":
+            with open("filtered_packets/" + filename, 'ab') as f:
+                f.write(packet_bytes)
     except ValueError as e:
         print("Value error:", e, file=sys.stderr)
     except AttributeError as e:
